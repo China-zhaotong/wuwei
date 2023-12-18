dda9aec9b59707eb0cfcdb08b4c53b1aaca60d72
zhaotong <1427730623@qq.com>
2023-12-18 16:33:27 +0800

    无为项目创建成功

---
+++ astro.config.mjs
@@ -0,0 +1,50 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [starlight({
+		title: '赵无为',
+		customCss: [
+			// 你的 Tailwind 基础样式的相对路径
+			'./src/tailwind.css',
+		],
+		locales: {
+			root: {
+				label: '简体中文',
+				lang: 'zh-CN',
+			},
+		},
+		logo: {
+			src: './src/assets/logo/logo.svg',
+		},
+		social: {
+			github: 'https://github.com/withastro/starlight'
+		},
+		sidebar: [{
+			label: 'Guides',
+			items: [
+				// Each item here is one entry in the navigation menu.
+				{
+					label: 'Example Guide',
+					link: '/guides/example/'
+				}]
+		}, {
+			label: 'Reference',
+			autogenerate: {
+				directory: 'reference'
+			}
+		},
+		{
+			label: '数据库知识',
+			autogenerate: {
+				directory: '数据库知识'
+			}
+		}]
+	}), tailwind({
+		// 禁用默认的基础样式
+		applyBaseStyles: false,
+	})]
+});
